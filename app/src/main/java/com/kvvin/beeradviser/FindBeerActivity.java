00439813e1dd46310dab2377015801cb10b7da6b
Kevin Thomas <kevinthomastm2002@gmail.com>
2023-04-10 15:26:48 +0530

    Initial commit

---
+++ app/src/main/java/com/kvvin/beeradviser/FindBeerActivity.java
@@ -0,0 +1,14 @@
+package com.kvvin.beeradviser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindBeerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activiyt_find_beer);
+    }
+}
