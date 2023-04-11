c1561e95a5327ce0f507dd8b0967ca3701a56af8
Kevin Thomas <kevinthomastm2002@gmail.com>
2023-04-11 22:36:33 +0530

    Initial commit

--- app/src/main/java/com/kvvin/beeradviser/FindBeerActivity.java
+++ app/src/main/java/com/kvvin/beeradviser/FindBeerActivity.java
@@ -1,14 +1,26 @@
 package com.kvvin.beeradviser;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class FindBeerActivity extends AppCompatActivity {
+public class FindBeerActivity extends Activity{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activiyt_find_beer);
     }
+
+    public void onClickFindBeer(View view){
+        TextView brands = (TextView) findViewById(R.id.brands);
+        Spinner color = (Spinner) findViewById(R.id.color);
+        String beerType = String.valueOf(color.getSelectedItem());
+        brands.setText(beerType);
+
+    }
+
 }
